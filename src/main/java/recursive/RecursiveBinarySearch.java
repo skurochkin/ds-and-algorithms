cd76e0a5b95f783e03b356b73c8719a5daba0ab1
Slav Kurochkin <slavkurochkin@Slavs-MacBook-Pro.local>
2017-03-08 00:04:15 -0500

    recursive binary search

--- src/main/java/recursive/RecursiveBinarySearch.java
+++ src/main/java/recursive/RecursiveBinarySearch.java
@@ -4,4 +4,24 @@ package recursive;
  * Created by VKurochkin on 3/7/2017.
  */
 public class RecursiveBinarySearch {
+
+    public static void main(String[] args) {
+        int[] listOfInt = {1,3,5,6,7,8,9};
+        System.out.println("Completed call: " + binarySearchRecursive(listOfInt,0,6,7));
+    }
+
+    public static int binarySearchRecursive(int[] a, int p, int r, int x){
+        if(p > r){
+            return -1;
+        }else {
+            int q = (p+r)/2;
+            if(a[q] == x){
+                return q;
+            }else if(a[q] > x){
+                return binarySearchRecursive(a, p,q-1,x);
+            }else{
+                return binarySearchRecursive(a,q+1,r, x);
+            }
+        }
+    }
 }

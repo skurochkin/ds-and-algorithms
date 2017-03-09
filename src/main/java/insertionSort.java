823013c8913a1e1f53e79fad65dc1c2d49593226
Slav Kurochkin <slavkurochkin@Slavs-MacBook-Pro.local>
2017-03-08 20:54:28 -0500

    InsertionSort

---
+++ src/main/java/insertionSort.java
@@ -0,0 +1,24 @@
+/**
+ * Created by slavkurochkin on 3/8/17.
+ */
+public class insertionSort {
+    public static void main(String[] args) {
+        int[] myArray = insertionSort(new int[] {9,8,3,13,87,12,19});
+        for (int i=0; i < myArray.length; i++){
+            System.out.println(myArray[i]);
+        }
+    }
+
+    public static int[] insertionSort(int[] a){
+        for (int i = 1; i < a.length; i++){
+            int element = a[i];
+            int j = i -1;
+            while (j >= 0 && a[j] > element){
+                a[j+1] = a[j];
+                j--;
+            }
+            a[j+1] = element;
+        }
+        return a;
+    }
+}
